3300fceb691db07b98823b095337c4c208ce47da
ym-pett <ympettinato@gmail.com>
2025-09-11 18:39:43 +0100

    series learning

--- narwhals_learning/series.py
+++ narwhals_learning/series.py
@@ -29,21 +29,19 @@ df_lazy = pl.LazyFrame({"a": [-1, 1, 3], "b": [3, 5, -3]})
 print(my_func1(df_lazy).collect())
 
 # to make a new column:
-#@nw.narwhalify - this is in the tutorial, do we need it? also, could use 'IntoFrameT' for both the function below but unsure if preferrable as is
+@nw.narwhalify # TODO: why do we need it? 
 def my_func2(df: FrameT) -> FrameT:
     return nw.from_native(df).with_columns((nw.col("a")*2).alias("double_a"))
 
 print(my_func2(df))
 
 # finding the mean of a column as a scalar
-# does this need to be `IntoDataFrameT`? also works with `IntoFrameT`
+# does this need to be `IntoDataFrameT`? Could it also works with `IntoFrameT`?
 
 def my_func3(df: IntoDataFrameT) -> float | None:
     return nw.from_native(df, eager_only=True)["a"].mean()
 
 print(my_func3(df))
-
-
 
 
 
